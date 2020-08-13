5e972888ec4ced63142596384479908cd6b6b4c6
QZhang <65087759+QZhang-Ing@users.noreply.github.com>
2020-08-13 08:37:03 +0200

    update source code

---
+++ source_1.py
@@ -0,0 +1,14 @@
+"""Contains source code under test for solution_ci."""
+
+def add_func(a, var_two):
+    """
+    Add two variables.
+
+    Args
+        a: first variable
+        var_two: second variable
+    Return
+        a + var_two
+
+    """
+    return a + var_two
